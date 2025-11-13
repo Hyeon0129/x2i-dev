d06cefed766302fa0c85c50cb1e31c6d4c14185f
김유빈 <mac@macui-MacBookPro.local>
2025-11-13 19:40:56 +0900

    timeline solid :none

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default function HomePage() {
         <SkillsSection /> {}
         <HistorySection /> {}
         <ProjectsSection /> {}
+        <div className="divider" />
       </div>
     </>
   )
